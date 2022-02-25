e3c25adfd6a327b5bba128c97266f2a3d7f7b089
cyberlancertw <cyberlancer@gmail.com>
2022-02-25 09:34:53 +0800

    B controller

---
+++ GitTest/Controllers/BController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GitTest.Controllers
+{
+    public class BController : Controller
+    {
+        // GET: B
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
